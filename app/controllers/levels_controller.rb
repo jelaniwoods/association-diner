857ef43b592d1d33ee1979712b2f61a94baf60fd
Jelani Woods <jelani@firstdraft.com>
2019-04-04 12:39:18 -0500

    Remove whitespace in input to match better
    Add a message if query returns a collection instead of an individual row

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -18,23 +18,27 @@ class LevelsController < ApplicationController
 
   def results
     @selections = @level.selections
-    @query = cookies[:query]
+    @query = cookies[:query].gsub(/\s+/, "")
     @res = false
+    @collection_returned = false
     # Selected One object
     # Selected Collection
     case @query
-    when "Plate.where(id: 2).first", "Plate.where(id: 2)[0]",
-      "Plate.where(:id => 2).first", "Plate.where({:id => 2}).first",
-      "Plate.find_by(id: 2)", "Plate.find_by(:id => 2)", "Plate.find(2)"
+    when "Plate.where(id:2).first", "Plate.where(id:2)[0]",
+      "Plate.where(:id=>2).first","Plate.where(:id=>2)[0]",
+      "Plate.where({:id=>2})[0]", "Plate.where({:id=>2}).first",
+      "Plate.find_by(id:2)", "Plate.find_by(:id=>2)", "Plate.find(2)",
       @res = true
       Selection.update(2, selected: true)
 
-  when "Plate.where({:id => 2})", "Plate.where(id: 2)", "Plate.where({:id=>2})",
-    "Plate.where(id:2)"
+  when "Plate.where({:id=>2})", "Plate.where(id:2)", "Plate.where({:id=>2})",
+    "Plate.where(id:2)", "Plate.where(:id=>2)"
       # TODO Select Collection
       # Selection.update(1, selected: true)
       # Selection.update(2, selected: true)
       # How to show that a Collection is returned an not a value
+      @collection_returned = true
+      @res = false
     else
       @res = false
     end
